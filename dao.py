50a29c27562abc572f807370085439996d3dd5d2
oldwhite233333 <84114842+oldwhite233333@users.noreply.github.com>
2021-05-28 19:24:32 +0800

    Add files via upload

--- dao.py
+++ dao.py
@@ -2,29 +2,26 @@ import openpyxl
 from fpdf import FPDF
 from PIL import Image
 
+
 class Dao:
     def __init__(self, eanlist, amount):
-        #print(eanlist)
-        #print(amount)
         self.eanlist = eanlist
         self.amount = amount
-        book = openpyxl.load_workbook('test.xlsx')
+        book = openpyxl.load_workbook('products.xlsx', data_only=True)
         dict = {}
         sheet = book.worksheets[0]
-        for row in sheet.rows:
-            innerSpec = Spec(row[6].value, row[7].value, row[8].value, row[9].value)
-            outerSpec = Spec(row[13].value, row[14].value, row[15].value, row[16].value)
-            product = Product(row[1].value,
-                              innerSpec, outerSpec, row[21].value)
-
-            dict[row[1].value] = product
+        rows = iter(sheet.rows)
+        next(rows)
+        self.productList = []
+        for row in rows:
+            product = Product(row)
+            self.productList.append(product)
+            dict[row[3].value] = product
         products = []
         boxs = []
         ean_to_asin = {}
-        for i in open('ean2sku.csv'):
-            k = i.split(',')
-            if i == 'SKU': continue
-            ean_to_asin[k[1]] = k[0]
+        for i in self.productList:
+            ean_to_asin[i.ean] = i.asin
         for i in eanlist:
             products.append(dict[i.strip()])
         for z in amount:
@@ -33,8 +30,7 @@ class Dao:
             boxs[i] = round(boxs[i] / products[i].unit_per_box)
         self.products = products
         self.boxs = boxs
-        self.ean_to_asin=ean_to_asin
-
+        self.ean_to_asin = ean_to_asin
 
 
 def to_inch(arg):
@@ -62,12 +58,40 @@ class Spec:
 
 
 class Product:
-    def __init__(self, ean,
-                 inner_spec, out_spec, unit_per_box):
-        self.ean = ean
-        self.inner_spec = inner_spec
-        self.out_spec = out_spec
-        self.unit_per_box = unit_per_box
+    def __init__(self, row, no="xlsx"):
+        if no == "":
+            self.no = no
+            self.name = row.name
+            self.asin = row.asin
+            self.model_id = row.model_id
+            self.ean = row.ean
+            self.rmb_price = row.rmb_price
+            self.usd_price = row.rmb_price / 6.5 if self.rmb_price else 0
+            self.ship_fee = row.ship_fee
+            self.item_amount = row.item_amount
+            self.inner_spec = Spec(row.inner_length, row.inner_width, row.inner_height, row.inner_weight)
+            self.out_spec = Spec(row.out_length, row.out_width, row.out_height, row.out_weight)
+            self.unit_per_box = row.unit_per_box
+        else:
+            self.row = row
+            self.name = self.v('a')
+            self.asin = self.v('b')
+            self.model_id = self.v('c')
+            self.ean = self.v('d')
+            self.rmb_price = self.v('e')
+            self.usd_price = self.rmb_price / 6.5 if self.rmb_price else 0
+            self.ship_fee = self.v('g')
+            self.item_amount = self.v('h')
+            self.inner_spec = Spec(self.v('i'), self.v('j'), self.v('k'), self.v('l'))
+            self.out_spec = Spec(self.v('p'), self.v('q'), self.v('r'), self.v('s'))
+            self.unit_per_box = self.v('x')
+
+    def v(self, s):
+        return self.row[ord(s) - 97].value
+
+
+def write_products(rowNum, product):
+    return
 
 
 def write_to_doc(products, boxs):
@@ -83,31 +107,44 @@ def write_to_doc(products, boxs):
     book.save('result.xlsx')
 
 
-def write_to_doc_amz(products, boxs):
-    k = 0
-    for i in products:
-        #print(i.ean)
-        # #print(ean_to_asin)
-        # #print(ean_to_asin[str(i.ean)])
-        #print(i.unit_per_box, boxs[k])
-        #print(i.out_spec)
-        # #print(i.inner_spec)
-        k += 1
-        #print('==============================')
-    # book = openpyxl.load_workbook('result.xlsx')
-    # sheet = book.worksheets[0]
-    # for i in range(0, len(boxs)):
-    #     sheet.cell(row=i + 1, column=1, value=products[i].unit_per_box)
-    #     sheet.cell(row=i + 1, column=2, value=boxs[i])
-    #     sheet.cell(row=i + 1, column=3, value=products[i].out_spec.get_volume())
-    #     sheet.cell(row=i + 1, column=4, value=products[i].out_spec.weight)
-    #     sheet.cell(row=i + 1, column=5, value=products[i].out_spec.get_volume() * boxs[i])
-    #     sheet.cell(row=i + 1, column=6, value=products[i].out_spec.weight * boxs[i])
-    # book.save('result.xlsx')
+def write_product_xlsx(product: Product, row):
+    # row+=1
+    row = 104
+    book = openpyxl.load_workbook('products.xlsx')
+    sheet = book.worksheets[0]
+
+    def s(i):
+        return ord(i) - 96
+
+    sheet.cell(row, column=s('a'), value=product.name)
+    sheet.cell(row, column=s('b'), value=product.asin)
+    sheet.cell(row, column=s('c'), value=product.model_id)
+    sheet.cell(row, column=s('d'), value=str(product.ean))
+    sheet.cell(row, column=s('e'), value=product.rmb_price)
+    sheet.cell(row, column=s('f'), value=round(product.rmb_price / 6.5, 2))
+    sheet.cell(row, column=s('g'), value=product.ship_fee)
+    sheet.cell(row, column=s('h'), value=product.item_amount)
+    sheet.cell(row, column=s('i'), value=product.inner_spec.length)
+    sheet.cell(row, column=s('j'), value=product.inner_spec.width)
+    sheet.cell(row, column=s('k'), value=product.inner_spec.height)
+    sheet.cell(row, column=s('l'), value=product.inner_spec.weight)
+    sheet.cell(row, column=s('m'), value=product.inner_spec.get_inch()[0])
+    sheet.cell(row, column=s('n'), value=product.inner_spec.get_inch()[1])
+    sheet.cell(row, column=s('o'), value=product.inner_spec.get_inch()[2])
+    sheet.cell(row, column=s('p'), value=product.out_spec.length)
+    sheet.cell(row, column=s('q'), value=product.out_spec.width)
+    sheet.cell(row, column=s('r'), value=product.out_spec.height)
+    sheet.cell(row, column=s('s'), value=product.out_spec.weight)
+    sheet.cell(row, column=s('t'), value=product.out_spec.get_inch()[0])
+    sheet.cell(row, column=s('u'), value=product.out_spec.get_inch()[1])
+    sheet.cell(row, column=s('v'), value=product.out_spec.get_inch()[2])
+    sheet.cell(row, column=s('w'), value=product.out_spec.get_pound())
+    sheet.cell(row, column=s('x'), value=product.unit_per_box)
+    sheet.cell(row, column=s('y'), value=product.out_spec.get_volume())
+    book.save('products.xlsx')
 
 
 def write_doc_xiongda(products, boxs):
-    #print(sum(boxs))
     book = openpyxl.load_workbook('result.xlsx')
     sheet = book.worksheets[0]
     for i in range(0, len(boxs)):
@@ -117,37 +154,32 @@ def write_doc_xiongda(products, boxs):
         sheet.cell(row=i + 1, column=7, value=products[i].out_spec.length)
         sheet.cell(row=i + 1, column=8, value=products[i].out_spec.width)
         sheet.cell(row=i + 1, column=9, value=products[i].out_spec.height)
-
     book.save('result.xlsx')
-
     return 0;
 
 
-def write_doc_plan(products, boxs, ean_to_asin,addr,planName):
+def write_doc_plan(products, boxs, ean_to_asin, addr, planName):
     plan_padding = 12
-    txtdir='inbound.txt'
+    txtdir = 'inbound.txt'
     if addr != 'hangzhou':
-        txtdir='inbound_xiongda.txt'
+        txtdir = 'inbound_xiongda.txt'
     plan = [i.split('\t') for i in open(txtdir)]
-    plan[0][1]=planName
+    plan[0][1] = planName
     for i in range(0, len(boxs)):
         plan[plan_padding + i][0] = ean_to_asin.get(products[i].ean, products[i].ean)
         plan[plan_padding + i][1] = str(products[i].unit_per_box)
         plan[plan_padding + i][2] = str(boxs[i])
         plan[plan_padding + i][3] = str(boxs[i] * products[i].unit_per_box)
-    # for i in plan:
-        #print(i)
     f = open('./shipment.txt', 'wb+')
     for i in plan:
         bet = '\t'
-        #print(i)
         f.write(bet.join(i).encode())
     f.close()
 
 
-def write_doc_shippment(dao,addr):
-    products=dao.products
-    boxs=dao.boxs
+def write_doc_shippment(dao, addr):
+    products = dao.products
+    boxs = dao.boxs
     book = openpyxl.load_workbook(addr)
     sheet = book.worksheets[0]
     # sheet.cell(row=5,column=14,value=9527);
@@ -167,20 +199,20 @@ def write_doc_shippment(dao,addr):
             # sheet.cell(row=14,column=col_padding+z,value=products[i].out_spec.get_volume())
             # sheet.cell()
     # for i in range(len(products)):
-        #print(products[i].unit_per_box, boxs[i])
-        #print(products[i].out_spec.get_pound())
-        #print(products[i].out_spec.get_inch())
-        #print('==================')
+    # print(products[i].unit_per_box, boxs[i])
+    # print(products[i].out_spec.get_pound())
+    # print(products[i].out_spec.get_inch())
+    # print('==================')
     book.save('shipment.xlsx')
     return
 
 
 def write_doc_shenzhen(products, boxs):
-    #print(sum(boxs))
+    # print(sum(boxs))
     book = openpyxl.load_workbook('result.xlsx')
     sheet = book.worksheets[0]
     count = 1;
-    #print(boxs)
+    # print(boxs)
     for i in range(len(boxs)):
         for z in range(boxs[i]):
             # #print(i)
@@ -197,25 +229,25 @@ def write_doc_shenzhen(products, boxs):
 
 
 def getInnerSpec(eanList):
-    #print(eanList)
+    # print(eanList)
     for i in eanList:
         i = i.strip('\n')
-        #print(i)
-        #print(dict[i].inner_spec.length)
-        #print(dict[i].inner_spec.width)
-        #print(dict[i].inner_spec.height)
-        #print(dict[i].inner_spec.weight)
-        #print("==============")
+        # print(i)
+        # print(dict[i].inner_spec.length)
+        # print(dict[i].inner_spec.width)
+        # print(dict[i].inner_spec.height)
+        # print(dict[i].inner_spec.weight)
+        # print("==============")
 
 
 def getEanBySku(eanlist, ean_to_asin):
-    #print(eanlist)
+    # print(eanlist)
     for z in eanlist:
         exist = False
         for i in ean_to_asin.keys():
             if ean_to_asin[i] == z:
                 exist = True
-                #print(i)
+                # print(i)
         # if (not exist): #print(z)
     # for i in eanlist:
     #     #print(list(ean_to_asin.keys())[list(ean_to_asin.values()).ean_to_asin(i)])
@@ -232,9 +264,6 @@ def dao_init(eanlist, amount):
                           innerSpec, outerSpec, row[21].value)
 
         dict[row[1].value] = product
-    # for i in dict.keys():
-    #     #print(dict[i].out_spec.get_volume())
-    # write_to_doc()
     products = []
     boxs = []
     for i in open('ean2sku.csv'):
@@ -247,45 +276,65 @@ def dao_init(eanlist, amount):
     for z in amount:
         boxs.append(int(z))
     for i in range(0, len(boxs)):
-        # #print(products[i].ean)
         boxs[i] = round(boxs[i] / products[i].unit_per_box)
-    # eanlist=['6973902051118','6973902051125','6973902051132','6973902051149']
-    # getInnerSpec(eanlist)
     write_doc_plan(products, boxs)
-    # getInnerSpec([i for i in open('eanlist')])
-    # write_to_doc_amz(products,boxs)
-    # write_doc_xiongda(products,boxs)
-    # write_doc_shippment(products,boxs)
-    # write_doc_shenzhen(products,boxs)
-    # write_to_doc(products,boxs)
-    # getEanBySku([i.strip('\n') for i in open('skulist')])
+
+
 def getFbaEanAndAmount(addr):
     book = openpyxl.load_workbook(addr)
     sheet = book.worksheets[0]
-    ean_padding_col=5
-    ean_padding_row=4
-    cur =0
-    eanlist=[]
-    qualityList=[]
+    ean_padding_col = 5
+    ean_padding_row = 4
+    cur = 0
+    eanlist = []
+    qualityList = []
     while 1:
-        ean=sheet[ean_padding_col+cur][ean_padding_row].value
-        if ean ==None: break
-        ean=ean.replace('EAN: ','')
-        quality=sheet[ean_padding_col+cur][ean_padding_row+4].value
-        cur+=1
+        ean = sheet[ean_padding_col + cur][ean_padding_row].value
+        if ean == None: break
+        ean = ean.replace('EAN: ', '')
+        quality = sheet[ean_padding_col + cur][ean_padding_row + 4].value
+        cur += 1
         eanlist.append(ean)
         qualityList.append(quality)
-    return (eanlist,qualityList)
+    return (eanlist, qualityList)
+
+
 def makePdf(ean):
-    ean=[i.replace('\n', '') + '-new.png' for i in ean]
-    pdfFileName='label.pdf'
-    cover = Image.open('./madeinchina/'+ean[0])
+    ean = [i.replace('\n', '') + '-new.png' for i in ean]
+    pdfFileName = 'label.pdf'
+    cover = Image.open('./madeinchina/' + ean[0])
     width, height = cover.size
     pdf = FPDF(unit="pt", format=[width, height])
     for page in ean:
         try:
             pdf.add_page()
-            pdf.image('./madeinchina/'+page, 0, 0)
+            pdf.image('./madeinchina/' + page, 0, 0)
         except Exception as e:
             continue
     pdf.output(pdfFileName, "F")
+
+
+def makeWorkFlowPlan(dao:Dao):
+    def s(i):
+        return ord(i) - 96
+    row_padding=9
+    book = openpyxl.load_workbook("workflow_template.xlsx")
+    sheet = book.worksheets[2]
+    for i in range(len(dao.products)):
+        print(i)
+        p=dao.products[i]
+        out_spec=p.out_spec.get_inch()
+        pound=p.out_spec.get_pound()
+        sheet.cell(row_padding+i,s('a'),value=p.asin)
+        sheet.cell(row_padding+i,s('b'),value=dao.amount[i])
+        sheet.cell(row_padding+i,s('f'),value=p.unit_per_box)
+        sheet.cell(row_padding+i,s('g'),value=dao.boxs[i])
+        sheet.cell(row_padding+i,s('h'),value=out_spec[0])
+        sheet.cell(row_padding+i,s('i'),value=out_spec[1])
+        sheet.cell(row_padding+i,s('j'),value=out_spec[2])
+        sheet.cell(row_padding+i,s('k'),value=pound)
+    book.save("workflow.xlsx")
+
+# if __name__ == '__main__':
+#     makeWorkFlowPlan()
+
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from typing import List
+import json
 
 import pandas as pd
 from fastapi import FastAPI, UploadFile, File, Request
 from starlette.middleware.cors import CORSMiddleware  #引入 CORS中间件模块
 from starlette.responses import FileResponse
 from pydantic import BaseModel
-from fastapi.templating import Jinja2Templates
-
 from dao import *
 
-templates = Jinja2Templates(directory="dist")
 
 app = FastAPI()
 origins = ["*"]
@@ -29,6 +27,25 @@ class fbaplan(BaseModel):
     amount: str
     addr: str
 
+class product(BaseModel):
+    row_num: int
+    ean: int
+    model_id:str
+    asin:str
+    name:str
+    rmb_price:float
+    ship_fee:float
+    item_amount:int
+    inner_length:float
+    inner_width:float
+    inner_height:float
+    inner_weight:float
+    out_length:float
+    out_width:float
+    out_height:float
+    out_weight:float
+    unit_per_box:int
+
 # @app.get("/")
 # async def serve_home(request: Request):
 #     return templates.TemplateResponse("index.html", {"request": request})
@@ -37,6 +54,35 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.post("/product")
+def set_product(form:product):
+    print(form)
+    write_product_xlsx(Product(form,""),form.row_num)
+    return 1
+
+@app.get("/product/{id}")
+def get_product(id:int):
+    # if id
+    sz=len(Dao([],[]).productList)
+    if id>=sz:
+        return None
+    product=Dao([],[]).productList[id-1]
+    tmp=product.__dict__
+    tmp.pop('row')
+    return tmp
+
+
+
+@app.get("/products")
+def getlist():
+    list=Dao([],[])
+    res={}
+    res['products']=[]
+    z=1
+    for i in list.productList:
+        res['products'].append({'num':z,'ean':i.ean,'model_id':i.model_id,'name':i.name})
+        z+=1
+    return res
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
@@ -53,12 +99,25 @@ def label():
 def ship():
     return FileResponse('./shipment.xlsx', filename='shipment.xlsx')
 
+@app.get("/echo/{text}")
+async def getEchoApi(text:str):
+    return {"echo":text}
+
+@app.get("/echo")
+async def getEchoApi():
+    return {"echo":"text"}
+@app.get("/workflow")
+def workflow():
+    return FileResponse('./workflow.xlsx', filename='workflow.xlsx')
+
 
 
 @app.post("/fbaplan")
 async def plan(fba:fbaplan):
     daoObject= Dao(fba.eanlist.strip('\n').split('\n'),fba.amount.strip('\n').split('\n'))
     write_doc_plan(daoObject.products,daoObject.boxs,daoObject.ean_to_asin,fba.addr,fba.planname)
+    makePdf(daoObject.eanlist)
+    makeWorkFlowPlan(daoObject)
     return FileResponse('./shipment.txt', filename='shipment.txt')
 @app.post('/upload')
 async def recv_file(file: UploadFile = File(...)):
@@ -75,6 +134,9 @@ async def recv_file(file: UploadFile = File(...)):
     write_doc_shippment(daoObject,file.filename)
     makePdf(newDao[0])
     return rt_msg
+
+
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='127.0.0.1', port=1099)
